ca4c41b9ae4f5c5e4a74e4c794a55506cfcc8c2e
teejohn247 <47560491+teejohn247@users.noreply.github.com>
2020-04-05 16:21:23 -0700

    Update userSignup.js

--- UI/xray/html/fetchApi/userSignup.js
+++ UI/xray/html/fetchApi/userSignup.js
@@ -8,7 +8,7 @@ const name = document.querySelector(".user_name");
 
 signup.addEventListener("submit", event => {
     fetch(
-    "http://localhost:5000/api/v1/register", {
+    "https://ems-employee-management-system.herokuapp.com/api/v1/register", {
         method: "POST",
 		headers: {
 			"Content-Type": "application/json"
